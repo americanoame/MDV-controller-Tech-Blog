bc7ff7c71e8b294a84451bd24126ea7956232c62
Americano.Dev <andrenations@gmail.com>
2023-03-08 09:38:32 -0500

    added the edit post page & fixed dashboard

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,20 +1,20 @@
-const loginFormHandler = async (event) => {
+const editPostHandler = async (event) => {
     event.preventDefault();
 
     const edit = document.querySelector('#editPost').value.trim();
-    const content = document.querySelector('#editConten').value.trim();
+    const content = document.querySelector('#editContent').value.trim();
     const editId = document.querySelector('#editId').value
 
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
+    if (title && content) {
+        const response = await fetch(`/api/users/${editId}`, {
+            method: 'PUT',
             body: JSON.stringify({ edit, content }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
-            // document.location.replace('/dashbord');
-            document.location.raplace(`/tech/${id}`);
+            // document.location.replace('/dashboard');
+            document.location.replace(`/tech/${editId}`);
         } else {
             alert('Failed to edit new post.');
         }
@@ -23,4 +23,4 @@ const loginFormHandler = async (event) => {
 
 document
     .querySelector('.form-horizontal')
-    .addEventListener('click', loginFormHandler);
+    .addEventListener('click', editPostHandler);
