181ffe7a4c6676190ab79de9eefafb4ec9a1443a
Americano.Dev <andrenations@gmail.com>
2023-03-11 06:54:20 -0500

    fixing the form

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,13 +1,12 @@
 const editPostHandler = async (event) => {
     event.preventDefault();
 
-    const edit = $('#editPost').val().trim();
-    const content = $('#editContent').val().trim();
-    const editId = $('#editId').val();
-
+    const title = document.querySelector('#editPost').value.trim();
+    const content = document.querySelector('#editContent').value.trim();
+    const edit_id = document.getElementById('#edit_id').value
 
     if (title && content) {
-        const response = await fetch(`/api/tech/${editId}`, {
+        const response = await fetch(`/api/tech/${edit_id}`, {
             method: 'PUT',
             body: JSON.stringify({ title, content }),
             headers: { 'Content-Type': 'application/json' },
@@ -15,7 +14,7 @@ const editPostHandler = async (event) => {
 
         if (response.ok) {
             // document.location.replace('/dashboard');
-            document.location.replace(`/tech/${editId}`);
+            document.location.replace(`/tech/${edit_id}`);
         } else {
             alert('Failed to edit new post.');
         }
@@ -23,5 +22,5 @@ const editPostHandler = async (event) => {
 };
 
 document
-    .querySelector('.tech_id')
+    .querySelector('.edit_tech')
     .addEventListener('click', editPostHandler);
